9bb6f41b2efd379293b79d3cfb7992d2756e5129
Oumar Mersile <oumarmersileattimahamat@yahoo.fr>
2023-04-15 06:11:46 +0200

    Step-1

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:santehaggi/views/login_view.dart';
 import 'package:santehaggi/views/register_view.dart';
 import 'package:rive/rive.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:santehaggi/views/verify_email_view.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -47,13 +48,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            /* final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                return const Text('Done');
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email verified');
               } else {
                 return const VerifyEmailView();
-              } */
-            return const LoginView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
           default:
             return const Text("Loading...");
         }
@@ -62,30 +67,7 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class VerifyEmailView extends StatefulWidget {
-  const VerifyEmailView({super.key});
 
-  @override
-  State<VerifyEmailView> createState() => _VerifyEmailViewState();
-}
-
-class _VerifyEmailViewState extends State<VerifyEmailView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Verify Email!')),
-      body: Column(children: [
-        const Text('Please verify your email:'),
-        TextButton(
-            onPressed: () async {
-              final user = FirebaseAuth.instance.currentUser;
-              await user?.sendEmailVerification();
-            },
-            child: const Text("Send Email verification"))
-      ]),
-    );
-  }
-}
 
 /* void main() {
   WidgetsFlutterBinding.ensureInitialized();
